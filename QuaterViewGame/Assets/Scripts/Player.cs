9fe037ae858dbbc580e80362f58a34029414c9f3
Zoselro <zoselro0328@gmail.com>
2025-09-15 21:50:24 +0900

    20250915 점프및 회피 구현

--- QuaterViewGame/Assets/Scripts/Player.cs
+++ QuaterViewGame/Assets/Scripts/Player.cs
@@ -17,13 +17,20 @@ public class Player : MonoBehaviour
     private Rigidbody rb;
 
     private float velocity;
-    private Vector3 rotation;
-    private Vector3 dodgeRotation;
+    private float baseSpeed; // 원래 속도 저장용
 
     private bool isWalk;
     private bool isRun;
     private bool isJump;
     private bool isDodge;
+    private bool keepMovingAfterDodge; // 회피를 시작 후 끝날 때 까지 플래그 유지
+    private bool keepMovingAfterJump; // 점프가 시작 하고 끝날 때 까지 플래그 유지
+
+    private Vector3 rotation;
+    private Vector3 rotation_value;
+    private Vector3 dodgeRotation;
+    private Vector3 dodgeMoveDir; // 회피동작이 끝날 때 까지 이동에 사용될 벡터
+    private Vector3 jumpMoveDir; // 점프동작이 끝날 때 까지 이동에 사용될 벡터
 
     private void Awake()
     {
@@ -32,13 +39,42 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-
+        baseSpeed = speed;
     }
 
     void FixedUpdate()
     {
-        Debug.Log(rotation);
-        Walking();
+        Move();
+    }
+
+    public void Move()
+    {
+        if (isDodge && keepMovingAfterDodge)
+        {
+            // 키보드를 떼도 dodgeMoveDir 로 이동
+            float moveSpeed = isDodge ? speed * Time.deltaTime
+                                      : baseSpeed * Time.deltaTime;
+            rb.linearVelocity = new Vector3(dodgeMoveDir.x * moveSpeed,
+                                            rb.linearVelocity.y,
+                                            dodgeMoveDir.y * moveSpeed);
+
+            transform.LookAt(transform.position + new Vector3(dodgeMoveDir.x, 0f, dodgeMoveDir.y));
+        }
+        else if (isJump && keepMovingAfterJump)
+        {
+            // 키보드를 떼도 dodgeMoveDir 로 이동
+            float moveSpeed = isJump ? speed * Time.deltaTime
+                                      : baseSpeed * Time.deltaTime;
+            rb.linearVelocity = new Vector3(jumpMoveDir.x * moveSpeed,
+                                            rb.linearVelocity.y,
+                                            jumpMoveDir.y * moveSpeed);
+
+            transform.LookAt(transform.position + new Vector3(jumpMoveDir.x, 0f, jumpMoveDir.y));
+        }
+        else
+        {
+            Walking();
+        }
     }
 
     public void Walking()
@@ -54,6 +90,8 @@ public class Player : MonoBehaviour
         if (context.performed)
         {
             rotation = context.ReadValue<Vector2>().normalized;
+            rotation_value = rotation;
+            // 만약에 회피가 동작이 끝났다면, 방향 입력값 다시 주기.
             if (isDodge)
                 rotation = dodgeRotation;
             isRun = true;
@@ -61,7 +99,8 @@ public class Player : MonoBehaviour
         else if (context.canceled)
         {
             isRun = false;
-            rotation = context.ReadValue<Vector2>().normalized;
+            rotation = Vector3.zero;
+            rotation_value = Vector3.zero;
         }
         animator.SetBool("IsRun", isRun);
     }
@@ -89,6 +128,8 @@ public class Player : MonoBehaviour
             isJump = true;
             animator.SetBool("IsJump", isJump);
             animator.SetTrigger("DoJump");
+            keepMovingAfterJump = true;
+            jumpMoveDir = rotation;
         }
     }
 
@@ -97,12 +138,15 @@ public class Player : MonoBehaviour
     {
         if (context.performed && rotation != Vector3.zero && !isJump && !isDodge)
         {
+            // 회피 시작 시 현재 입력 방향을 그대로 저장
+            dodgeMoveDir = rotation;
+            // 만약에, 회피중이라면, 지금 보는 방향 그대로 직진.
             dodgeRotation = rotation;
             speed *= 2;
             isDodge = true;
             animator.SetTrigger("DoDodge");
 
-            Invoke("DodgeOut", 0.5f);
+            Invoke("DodgeOut", 0.5f); // 회피가 끝났을 때 수행되는 함수.
         }
     }
         
@@ -110,6 +154,8 @@ public class Player : MonoBehaviour
     {
         speed *= 0.5f;
         isDodge = false;
+        rotation = rotation_value;
+        keepMovingAfterDodge = true;
     }
 
     private void OnCollisionEnter(Collision collision)
