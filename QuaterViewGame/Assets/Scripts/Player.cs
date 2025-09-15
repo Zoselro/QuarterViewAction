1a7e589f9c53a080ddff89d5c4a1d18ab0daa992
simgyujin <91524226+Zoselro@users.noreply.github.com>
2025-09-15 18:25:07 +0900

    회피동작 구현

--- QuaterViewGame/Assets/Scripts/Player.cs
+++ QuaterViewGame/Assets/Scripts/Player.cs
@@ -4,18 +4,26 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
 
+    [Header("Options")]
     [SerializeField]
     private float speed;
     [SerializeField]
-    private Rigidbody rb;
+    private float jumpPower;
+
+    [Header("Components")]
     [SerializeField]
     private Animator animator;
+    [SerializeField]
+    private Rigidbody rb;
 
     private float velocity;
-    private Vector3 inputValue;
+    private Vector3 rotation;
+    private Vector3 dodgeRotation;
+
     private bool isWalk;
     private bool isRun;
-    private Vector3 rotation;
+    private bool isJump;
+    private bool isDodge;
 
     private void Awake()
     {
@@ -29,27 +37,36 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        velocity = isWalk ? speed * 0.3f * Time.deltaTime : speed * Time.deltaTime;
-        rb.linearVelocity = new Vector3(inputValue.x * velocity, 0f, inputValue.y * velocity);
-        transform.LookAt(transform.position + rotation);
+        Debug.Log(rotation);
+        Walking();
     }
 
+    public void Walking()
+    {
+        velocity = isWalk ? speed * 0.3f * Time.deltaTime : speed * Time.deltaTime;
+        rb.linearVelocity = new Vector3(rotation.x * velocity, rb.linearVelocity.y , rotation.y * velocity);
+        transform.LookAt(transform.position + new Vector3(rotation.x, 0f, rotation.y));
+    }
+
+    // 방향키를 눌렀을 때 실행되는 메서드
     public void PlayerMove(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
+            rotation = context.ReadValue<Vector2>().normalized;
+            if (isDodge)
+                rotation = dodgeRotation;
             isRun = true;
-            inputValue = context.ReadValue<Vector2>().normalized;
-            rotation = new Vector3(inputValue.x, 0f, inputValue.y);
         }
         else if (context.canceled)
         {
             isRun = false;
-            inputValue = context.ReadValue<Vector2>().normalized;
+            rotation = context.ReadValue<Vector2>().normalized;
         }
         animator.SetBool("IsRun", isRun);
     }
 
+    // 왼쪽 쉬프트키를 눌렀을때 실행되는 메서드
     public void PlayerWalk(InputAction.CallbackContext context)
     {
         if (context.performed)
@@ -61,5 +78,46 @@ public class Player : MonoBehaviour
             isWalk = false;
         }
         animator.SetBool("IsWalk", isWalk);
+    }
+
+    // 스페이스바를 눌렀을 때 실행되는 점프 메서드
+    public void Jumb(InputAction.CallbackContext context)
+    {
+        if (context.performed && /*rotation == Vector3.zero &&*/ !isJump && !isDodge)
+        {
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            isJump = true;
+            animator.SetBool("IsJump", isJump);
+            animator.SetTrigger("DoJump");
+        }
+    }
+
+    // 컨트롤키를 눌렀을 때 실행되는 회피 메서드
+    public void Dodge(InputAction.CallbackContext context)
+    {
+        if (context.performed && rotation != Vector3.zero && !isJump && !isDodge)
+        {
+            dodgeRotation = rotation;
+            speed *= 2;
+            isDodge = true;
+            animator.SetTrigger("DoDodge");
+
+            Invoke("DodgeOut", 0.5f);
+        }
+    }
+        
+    private void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            isJump = false;
+            animator.SetBool("IsJump", isJump);
+        }
     }
 }
